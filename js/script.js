1ea08a34a8c0a453e7a4b7eba2ffff7fb50344a9
Wady Paulino <wadymunguambe73@gmail.com>
2024-01-11 21:01:56 +0200

    feat: imprime as mensagens de validacao e muda o icone do cadeado de acordo com a acertividade ou nao do PIN

--- js/script.js
+++ js/script.js
@@ -1,7 +1,39 @@
 var PIN = 1234;
+var imgs = ['./imgs/locked.png', './imgs/unlocked.png']
 
 function errorMessage(message) {
 	document.querySelector('.mensagem').innerHTML = `<li>${message}</li>`;
+}
+
+function passedMessage(message) {
+	document.querySelector('.mensagem').innerHTML = `<strong style="color: green;">${message} &check;</strong>`;
+}
+
+function verifica_proximidade_PIN(getPIN, digitos) {
+	if(getPIN < PIN && digitos < 4) {
+		if(getPIN > PIN - 100) {
+			errorMessage(`TENTE UM PIN UM POUCO MAIOR. O PIN DEVE TER 4 DÍGITOS`);
+		}
+		else {
+			errorMessage(`TENTE UM PIN MAIOR. O PIN DEVE TER 4 DÍGITOS`)
+		}
+	} 
+	else if(getPIN > PIN && digitos > 4) {
+		if(getPIN < PIN - 100) {
+			errorMessage(`TENTE UM PIN UM POUCO MENOR. O PIN DEVE TER 4 DÍGITOS`);
+		}
+		else {
+			errorMessage(`TENTE UM PIN MENOR. O PIN DEVE TER 4 DÍGITOS`)
+		}
+	}
+	else {
+		if(getPIN < PIN) {
+			errorMessage(`TENTE UM PIN UM POUCO MAIOR.`);
+		}
+		else {
+			errorMessage(`TENTE UM PIN UM POUCO MENOR. `)
+		}
+	}
 }
 
 document.querySelector('.submit-pin-button').
@@ -23,15 +55,21 @@ document.querySelector('.submit-pin-button').
 			} 
 
 			if(digitos == 4) {
-				console.log('O pin informado tem 4 digitos!');
+					if(getPIN == PIN) {
+						passedMessage(`PASSED`);
+						document.querySelector('.main-result-field')
+							.innerHTML = `<img class="locked-unlocked-icons" src="${imgs[1]}">Desbloqueado`;
+					}
+					else {
+						verifica_proximidade_PIN(getPIN, digitos);
+						document.querySelector('.main-result-field')
+						.innerHTML = `<img class="locked-unlocked-icons" src="${imgs[0]}">Bloqueado`;
+					}
 			}
 			else {
-				if(getPIN < PIN && digitos < 4) {
-					errorMessage(`TENTE UM PIN MAIOR. O PIN DEVE TER 4 DÍGITOS`);
-				} 
-			  else if(getPIN > PIN && digitos > 4) {
-					errorMessage(`TENTE UM PIN MENOR. O PIN DEVE TER 4 DÍGITOS`)
-				}
+				verifica_proximidade_PIN(getPIN, digitos);
+				document.querySelector('.main-result-field')
+				.innerHTML = `<img class="locked-unlocked-icons" src="${imgs[0]}">Bloqueado`;
 			}
 		}
 	})
